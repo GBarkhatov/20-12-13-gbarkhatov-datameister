947ba25683fa0871c4842b45cfbb7d10ee2871bd
Govard Barkhatov <gbarkhatov@gmail.com>
2020-12-13 18:57:48 +1000

    add basic styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react'
 import styled from 'styled-components/macro'
+import { Radio } from 'antd'
 
-const Component = styled.div``
+const Component = styled.div`
+  padding: 1rem;
+
+  @media screen and (min-width: 768px) {
+    padding: 3rem;
+  }
+`
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
 const App = () => {
   const [inputMode, setInputMode] = useState(true)
 
   return (
     <Component>
+      <ButtonsWrapper>
+        <Radio.Group>
+          <Radio.Button value='small'>Input</Radio.Button>
+          <Radio.Button value='default' disabled>
+            Output
+          </Radio.Button>
+        </Radio.Group>
+      </ButtonsWrapper>
       Inside App
     </Component>
   )
