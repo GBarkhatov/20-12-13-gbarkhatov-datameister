8997bc34c5f68f383b37fb314052a484d5acffb6
Govard Barkhatov <gbarkhatov@gmail.com>
2020-12-13 21:39:43 +1000

    add graph

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import { Typography, Row, Col, Divider, Table } from 'antd'
+import { ResponsiveBar } from '@nivo/bar'
 
 const Component = styled.div``
+
+const GraphWrapper = styled.div`
+  height: 655px;
+  width: 100%;
+`
 
 const ColWrapper = styled.div`
   display: flex;
@@ -98,6 +104,76 @@ const Output = ({ name, gender, age, email, country, city, csv }) => {
         </Col>
         <Col xs={24} md={12}>
           <Typography.Title level={4}>Graph</Typography.Title>
+          <GraphWrapper>
+            <ResponsiveBar
+              data={dataSource}
+              keys={[columns[1].title]}
+              indexBy={columns[0].title}
+              margin={{ top: 50, right: 50, bottom: 100, left: 60 }}
+              padding={0.3}
+              valueScale={{ type: 'linear' }}
+              indexScale={{ type: 'band', round: true }}
+              colors={{ scheme: 'nivo' }}
+              defs={[
+                {
+                  id: 'dots',
+                  type: 'patternDots',
+                  background: 'inherit',
+                  color: '#38bcb2',
+                  size: 4,
+                  padding: 1,
+                  stagger: true
+                },
+                {
+                  id: 'lines',
+                  type: 'patternLines',
+                  background: 'inherit',
+                  color: '#eed312',
+                  rotation: -45,
+                  lineWidth: 6,
+                  spacing: 10
+                }
+              ]}
+              fill={[
+                {
+                  match: {
+                    id: 'fries'
+                  },
+                  id: 'dots'
+                },
+                {
+                  match: {
+                    id: 'sandwich'
+                  },
+                  id: 'lines'
+                }
+              ]}
+              borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                legendPosition: 'middle',
+                tickRotation: -90,
+                legendOffset: 32
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: columns[1].title,
+                legendPosition: 'middle',
+                legendOffset: -40
+              }}
+              labelSkipWidth={12}
+              labelSkipHeight={12}
+              labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+              animate
+              motionStiffness={90}
+              motionDamping={15}
+            />
+          </GraphWrapper>
         </Col>
       </Row>
     </Component>
