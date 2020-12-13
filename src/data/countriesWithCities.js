0807dc483f2610db01d4979ba877352611507d55
Govard Barkhatov <gbarkhatov@gmail.com>
2020-12-13 18:47:42 +1000

    add countries with cities

---
+++ src/data/countriesWithCities.js
@@ -0,0 +1,183 @@
+const countriesWithCities = [
+  {
+    name: 'Austria',
+    cities: ['WIEN (Vienna)']
+  },
+  {
+    name: 'Belarus',
+    cities: ['MINSK']
+  },
+  {
+    name: 'Bosnia',
+    cities: ['SARAJEVO']
+  },
+  {
+    name: 'Bulgaria',
+    cities: ['SOFIA']
+  },
+  {
+    name: 'Croatia',
+    cities: ['ZAGREB']
+  },
+  {
+    name: 'Czech Republic',
+    cities: ['PRAHA (Prague)']
+  },
+  {
+    name: 'Denmark',
+    cities: ['KOBENHAVN (Copenhagen)']
+  },
+  {
+    name: 'Finland',
+    cities: ['HELSINKI']
+  },
+  {
+    name: 'France',
+    cities: ['PARIS', 'Marseille']
+  },
+  {
+    name: 'Germany',
+    cities: [
+      'BERLIN',
+      'Hamburg',
+      'München (Munich)',
+      'Köln (Cologne)',
+      'Frankfurt am Main',
+      'Essen',
+      'Dortmund',
+      'Stuttgart',
+      'Düsseldorf',
+      'Bremen',
+      'Duisburg',
+      'Hannover'
+    ]
+  },
+  {
+    name: 'Greece',
+    cities: ['ATHINAI (Athens)', 'Salonika']
+  },
+  {
+    name: 'Hungary',
+    cities: ['BUDAPEST']
+  },
+  {
+    name: 'Italy',
+    cities: [
+      'ROMA',
+      'Milano (Milan)',
+      'Napoli (Naples)',
+      'Torino (Turin)',
+      'Palermo',
+      'Genova'
+    ]
+  },
+  {
+    name: 'Latvia',
+    cities: ['RIGA']
+  },
+  {
+    name: 'Lithuania',
+    cities: ['VILNIUS']
+  },
+  {
+    name: 'Moldova',
+    cities: ['KISHINEV']
+  },
+  {
+    name: 'Netherlands',
+    cities: ['AMSTERDAM', 'Rotterdam']
+  },
+  {
+    name: 'Norway',
+    cities: ['OSLO']
+  },
+  {
+    name: 'Poland',
+    cities: [
+      'WARSZAWA (Warsaw)',
+      'Lódz',
+      'Kraków',
+      'Wroclaw (Breslau)',
+      'Poznan'
+    ]
+  },
+  {
+    name: 'Portugal',
+    cities: ['LISBOA (Lisbon)']
+  },
+  {
+    name: 'Romania',
+    cities: ['BUCURESTI (Bucharest)']
+  },
+  {
+    name: 'Russia',
+    cities: [
+      'MOSKVA (Moscow)',
+      'St Petersburg',
+      'Novosibirsk',
+      'Nizhny Novgorod',
+      'Ekaterinoburg',
+      'Samara',
+      'Omsk',
+      'Kazan',
+      'Ufa',
+      'Chelyabinsk',
+      'Perm',
+      'Rostov-na-Donu',
+      'Volgograd',
+      'Voronezh',
+      'Krasnoyarsk',
+      'Saratov',
+      'Tolyatti',
+      'Ulyanovsk',
+      'Izhevsk',
+      'Krasnodar',
+      'Yaroslave',
+      'Khabarovsk',
+      'Vladivostok',
+      'Irkutsk',
+      'Barnaul',
+      'Novokuznetsk',
+      'Penza',
+      'Ryazan',
+      'Orenburg',
+      'Naberezhnye Tchelny',
+      'Lipetsk',
+      'Tula',
+      'Tyumen',
+      'Kemerovo'
+    ]
+  },
+  {
+    name: 'Serbia',
+    cities: ['BEOGRAD (Belgrade)']
+  },
+  {
+    name: 'Spain',
+    cities: ['MADRID', 'Barcelona', 'Valencia', 'Sevilla', 'Zaragoza', 'Málaga']
+  },
+  {
+    name: 'Sweden',
+    cities: ['STOCKHOLM']
+  },
+  {
+    name: 'UK',
+    cities: ['LONDON', 'Birmingham', 'Leeds', 'Glasgow', 'Sheffield']
+  },
+  {
+    name: 'Ukraine',
+    cities: [
+      'KIEV',
+      'Kharkov',
+      'Dnepropetrovsk',
+      'Donetsk',
+      'Odessa',
+      'Zaporozhye',
+      'Lvov',
+      'Kryvy Rig',
+      'Mykolaiv'
+    ]
+  }
+]
+
+export default countriesWithCities
